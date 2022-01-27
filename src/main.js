d9c91e61f45983bd47b0f33d522d89b95a3e06fa
ryouzhe <sjh3183@naver.com>
2022-01-27 14:43:23 +0900

    Front App Server 연동

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -36,6 +40,16 @@ app.use(bodyParser());
 app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+    // Not Found이고, 주소가 /api로 시작하지 않는 경우
+    if(ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+        // index.html 내용을 반환
+        await send(ctx, 'index.html', { root: buildDirectory });
+    }
+})
 
 const port = PORT || 4000;
 app.listen(port, () => {
