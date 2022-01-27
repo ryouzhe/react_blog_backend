880261d8a5943aaa1066e0f3d834cab5b7914068
ryouzhe <sjh3183@naver.com>
2022-01-27 10:36:45 +0900

    sanitizeHtml 적용

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,8 +1,39 @@
 import Post from "../../models/post";
 import mongoose from "mongoose";
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+    allowedTags: [
+      'h1',
+      'h2',
+      'b',
+      'i',
+      'u',
+      's',
+      'p',
+      'ul',
+      'ol',
+      'li',
+      'blockquote',
+      'a',
+      'img',
+    ],
+    allowedAttributes: {
+      a: ['href', 'name', 'target'],
+      img: ['src'],
+      li: ['class'],
+    },
+    allowedSchemes: ['data', 'http'],
+  };
+const removeHtmlAndShorten = body => {
+    const filtered = sanitizeHtml(body, {
+        allowedTags: [],
+    });
+    return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
+};
 
 export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
@@ -44,9 +75,7 @@ export const write = async ctx => {
     const schema = Joi.object().keys({
         title: Joi.string().required(),
         body: Joi.string().required(),
-        tags: Joi.array()
-            .items(Joi.string())
-            .required(),
+        tags: Joi.array().items(Joi.string()).required(),
     });
 
     const result = schema.validate(ctx.request.body);
@@ -59,7 +88,7 @@ export const write = async ctx => {
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
-        body,
+        body: sanitizeHtml(body, sanitizeOption),
         tags,
         user: ctx.state.user,
     });
@@ -97,8 +126,7 @@ export const list = async ctx => {
         ctx.set('Last-Page', Math.ceil(postCount / 10));
         ctx.body = posts.map(post => ({
             ...post,
-            body:
-            post.body.length < 200 ? post.body : `${post.body.slice(0,200)}...`,
+            body: removeHtmlAndShorten(post.body),
         }))
     } catch (e) {
         ctx.throw(500, e);
@@ -145,16 +173,22 @@ export const update = async ctx => {
         return;
     }
 
+    const nextData = { ...ctx.request.body }; // 객체를 복사하고
+    // body 값이 주어졌으면 HTML 필터링
+    if (nextData.body) {
+      nextData.body = sanitizeHtml(nextData.body, sanitizeOption);
+    }
     try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-            new: true,
+        const post = await Post.findByIdAndUpdate(id, nextData, {
+          new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+          // false 일 때에는 업데이트 되기 전의 데이터를 반환합니다.
         }).exec();
-        if(!post) {
-            ctx.status = 404;
-            return;
+        if (!post) {
+          ctx.status = 404;
+          return;
         }
         ctx.body = post;
-    } catch (e) {
+      } catch (e) {
         ctx.throw(500, e);
-    }
+      }
 };
